8a00b934ba0be6f771c50b89d7889ae9d88878cc
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2023-06-17 13:54:20 +0200

    NodeJS server ;)

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const upload = multer();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const file = req.file;
+  if (!file || file.mimetype !== 'application/x-msdownload') {
+    res.status(400).send('Invalid file');
+    return;
+  }
+
+  const fileContent = req.file.buffer.toString('binary');
+
+  // Check if the file contains the instance of "http://www.boomlings.com/database"
+  if (!fileContent.includes('http://www.boomlings.com/database')) {
+    res.status(400).send('File does not contain "http://www.boomlings.com/database"! Did you use a gdps?');
+    return;
+  }
+
+  let modifiedContent = fileContent;
+  modifiedContent = modifiedContent.replace(/http:\/\/www\.boomlings\.com\/database/g, 'https://dindegmdps.us.to/database');
+  modifiedContent = modifiedContent.replace(/aHR0cDovL3d3dy5ib29tbGluZ3MuY29tL2RhdGFiYXNl/g, 'aHR0cHM6Ly9kaW5kZWdtZHBzLnVzLnRvL2RhdGFiYXNl');
+
+  const modifiedFile = Buffer.from(modifiedContent, 'binary');
+
+  res.setHeader('Content-Disposition', 'attachment; filename=DindeGDPS.exe');
+  res.setHeader('Content-Type', 'application/x-msdownload');
+  res.send(modifiedFile);
+});
+
+app.listen(6973, () => {
+  console.log(`Server is running on port 6973`);
+});
