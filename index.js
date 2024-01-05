ce475abff21c779d48397151c6c97bf76946f42f
Jean <63162857+masckmaster2007@users.noreply.github.com>
2024-01-05 22:25:03 +0100

    2.2 support

--- index.js
+++ index.js
@@ -19,14 +19,18 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
   const fileContent = req.file.buffer.toString('binary');
 
+  let modifiedContent = fileContent;
+
   // Check if the file contains the instance of "http://www.boomlings.com/database"
-  if (!fileContent.includes('http://www.boomlings.com/database')) {
+  if (!fileContent.includes('www.boomlings.com/database')) {
     res.status(400).send('File does not contain "http://www.boomlings.com/database"! Did you use a gdps?');
     return;
+  } else if(modifiedContent.includes('http://www.boomlings.com/database')) {
+    modifiedContent = modifiedContent.replace(/http:\/\/www\.boomlings\.com\/database/g, 'https://dindegmdps.us.to/database');
+  } else {
+    modifiedContent = modifiedContent.replace(/https:\/\/www\.boomlings\.com\/database/g, 'https://dindegmdps.us.to//database');
   }
 
-  let modifiedContent = fileContent;
-  modifiedContent = modifiedContent.replace(/http:\/\/www\.boomlings\.com\/database/g, 'https://dindegmdps.us.to/database');
   modifiedContent = modifiedContent.replace(/aHR0cDovL3d3dy5ib29tbGluZ3MuY29tL2RhdGFiYXNl/g, 'aHR0cHM6Ly9kaW5kZWdtZHBzLnVzLnRvL2RhdGFiYXNl');
 
   const modifiedFile = Buffer.from(modifiedContent, 'binary');
@@ -36,6 +40,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
   res.send(modifiedFile);
 });
 
-app.listen(2000, () => {
-  console.log(`Server is running on port 2000`);
+app.listen(8100, () => {
+  console.log(`Server is running on port lol`);
 });
